507a8b30405fae752852dde159240a793becbe5a
Matthew Brown <github@muglug.com>
2018-06-05 21:30:21 -0400

    Add Requests classloader in bootstrapper

--- bootstrap.php
+++ bootstrap.php
@@ -2,3 +2,16 @@
 
 define( 'WPINC', 'wp-includes' );
 define( 'ABSPATH', __DIR__ . '/src/');
+
+// register the Requests 
+spl_autoload_register(function ($class) {
+	// Check that the class starts with "Requests"
+	if (strpos($class, 'Requests') !== 0) {
+		return;
+	}
+
+	$file = str_replace('_', '/', $class);
+	if (file_exists(ABSPATH . WPINC . '/' . $file . '.php')) {
+		require_once(ABSPATH . WPINC . '/' . $file . '.php');
+	}
+});
