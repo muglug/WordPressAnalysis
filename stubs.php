f900879e46e15281ecd5900b33bda7c694083170
Matthew Brown <github@muglug.com>
2018-06-13 00:24:48 -0400

    Ignore some files, fix function treatment

--- stubs.php
+++ stubs.php
@@ -10,25 +10,3 @@
 function is_wp_error ($thing) {
 	return ( $thing instanceof WP_Error );
 }
-
-/**
- * @template T
- *
- * @param T $value
- * @return T
- */
-function wp_slash( $value ) {
-	if ( is_array( $value ) ) {
-		foreach ( $value as $k => $v ) {
-			if ( is_array( $v ) ) {
-				$value[ $k ] = wp_slash( $v );
-			} else {
-				$value[ $k ] = addslashes( $v );
-			}
-		}
-	} else {
-		$value = addslashes( $value );
-	}
-
-	return $value;
-}
