f08ff1e259df482a2b4dee0aec52a46b67151930
Matt Brown <github@muglug.com>
2020-11-26 12:26:54 -0500

    Add taint specialisation to add_filters

--- stubs.php
+++ stubs.php
@@ -94,3 +94,45 @@ function esc_attr( $text ) {}
  *  )
  */
 function get_post( $post = null, $output = OBJECT, $filter = 'raw' ) {}
+
+/**
+ * Calls the callback functions that have been added to a filter hook.
+ *
+ * The callback functions attached to the filter hook are invoked by calling
+ * this function. This function can be used to create a new filter hook by
+ * simply calling this function with the name of the new hook specified using
+ * the `$tag` parameter.
+ *
+ * The function also allows for multiple additional arguments to be passed to hooks.
+ *
+ * Example usage:
+ *
+ *     // The filter callback function.
+ *     function example_callback( $string, $arg1, $arg2 ) {
+ *         // (maybe) modify $string.
+ *         return $string;
+ *     }
+ *     add_filter( 'example_filter', 'example_callback', 10, 3 );
+ *
+ *     /*
+ *      * Apply the filters by calling the 'example_callback()' function
+ *      * that's hooked onto `example_filter` above.
+ *      *
+ *      * - 'example_filter' is the filter hook.
+ *      * - 'filter me' is the value being filtered.
+ *      * - $arg1 and $arg2 are the additional arguments passed to the callback.
+ *     $value = apply_filters( 'example_filter', 'filter me', $arg1, $arg2 );
+ *
+ * @since 0.71
+ *
+ * @global WP_Hook[] $wp_filter         Stores all of the filters and actions.
+ * @global string[]  $wp_current_filter Stores the list of current filters with the current one last.
+ *
+ * @param string $tag     The name of the filter hook.
+ * @param mixed  $value   The value to filter.
+ * @param mixed  ...$args Additional parameters to pass to the callback functions.
+ * @return mixed The filtered value after all hooked functions are applied to it.
+ *
+ * @psalm-taint-specialize
+ */
+function apply_filters( $tag, $value ) {}
